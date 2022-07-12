eca8ee77d3605931bf6c2b33be323af5828ff29a
Re-Dnor <MokshinRE@yandex.ru>
2022-07-12 14:13:23 +0500

    init project: countries

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import axios from "axios";
+import { rootReducer } from './rootReducer';
+import * as api from '../config'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(
+    thunk.withExtraArgument({
+      client: axios,
+      api,
+    })
+  )
+))
+
+export default store;
