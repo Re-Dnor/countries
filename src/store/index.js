4a3c41c0adc4bd646c5921cbd21cc2b1486d730e
Re-Dnor <MokshinRE@yandex.ru>
2022-07-13 15:33:57 +0500

    remove blacklist

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['theme']
+  // blacklist: ['']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
